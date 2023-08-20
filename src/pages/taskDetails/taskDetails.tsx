1a06a5fb4ce389f96bc9bfa00693b64c58db415e
snehajoy05 <snehajoy789@gmail.com>
2023-08-21 01:45:23 +0530

    added-the-comment-component

--- src/pages/taskDetails/taskDetails.tsx
+++ src/pages/taskDetails/taskDetails.tsx
@@ -49,6 +49,22 @@ const TaskDetails = () => {
           </div>
         </>
       </div>
+      <div className='progress-subheader'>
+        <div className='progress'>Progress</div>
+      </div>
+      <div className='commentBox'>
+        <div className='comment-text' contentEditable='true'>
+          <textarea className='comment-textbox' placeholder='Type your message...'></textarea>
+        </div>
+        <div className='send'>
+          <button className='file-button'>
+            <img src='assets/img/add.png' />
+          </button>
+          <button className='send-button'>
+            <img src='assets/img/send.png' />
+          </button>
+        </div>
+      </div>
     </Layout>
   );
 };
