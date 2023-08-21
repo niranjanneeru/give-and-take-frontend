46e3ef5effc3761b60ed112177ef9f20248e485e
Niranjan B <niranjannb7777@gmail.com>
2023-08-21 14:41:41 +0530

    Comment Input Made into a Component

--- src/pages/taskDetails/taskDetails.tsx
+++ src/pages/taskDetails/taskDetails.tsx
@@ -6,6 +6,7 @@ import { useNavigate, useParams } from 'react-router-dom';
 import ReactMarkdown from 'react-markdown';
 import ParticipantList from '../../components/participants/participants';
 import { useGetTaskByIDQuery } from './api';
+import CommentInput from '../../components/commentInput/commentInput';
 
 const TaskDetails = () => {
   const [icon] = useState('pencil');
@@ -56,19 +57,7 @@ const TaskDetails = () => {
       <div className='progress-subheader'>
         <div className='progress'>Progress</div>
       </div>
-      <div className='commentBox'>
-        <div className='comment-text' contentEditable='true'>
-          <textarea className='comment-textbox' placeholder='Type your message...'></textarea>
-        </div>
-        <div className='send'>
-          <button className='file-button'>
-            <img src='assets/img/add.png' />
-          </button>
-          <button className='send-button'>
-            <img src='assets/img/send.png' />
-          </button>
-        </div>
-      </div>
+      <CommentInput></CommentInput>
     </Layout>
   );
 };
