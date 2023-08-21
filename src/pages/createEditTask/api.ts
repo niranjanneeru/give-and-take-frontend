de57be86807a0d5a107533ac01f79eda23fa8cd1
Sruthy M L <56227030+sru-thy@users.noreply.github.com>
2023-08-21 15:56:25 +0530

    Create task integration (#21)
    * create-edit-task page
    
    * integrate create employee
    
    ---------
    
    Co-authored-by: Vaishnav N T <68477941+Vaishnav-NT@users.noreply.github.com>
    Co-authored-by: Sruthy <sruthyml328@gmail.com>

---
+++ src/pages/createEditTask/api.ts
@@ -0,0 +1,26 @@
+import baseApi from '../../services/index';
+
+const createTaskApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    createTask: builder.mutation({
+      query: (body) => ({
+        url: 'tasks',
+        method: 'POST',
+        body
+      }),
+      invalidatesTags: ['task.list']
+    }),
+    updateTask: builder.mutation({
+      query: ({ id, ...patch }) => ({
+        url: `tasks/${id}`,
+        method: 'PATCH',
+        body: patch
+      }),
+      invalidatesTags: ['task.list']
+    })
+  })
+});
+
+export default createTaskApi;
+
+export const { useCreateTaskMutation, useUpdateTaskMutation } = createTaskApi;
