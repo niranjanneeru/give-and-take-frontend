5ca7ec881bdba9495249b0dd6822e03567b08c64
Vaishnav N T <68477941+Vaishnav-NT@users.noreply.github.com>
2023-08-21 10:57:45 +0530

    clean code

--- src/pages/task/task.tsx
+++ src/pages/task/task.tsx
@@ -9,39 +9,8 @@ const TaskListPage = () => {
   const [icon] = useState('pencil');
   //const navigate = useNavigate();
 
+  // add use effect
   const { data: taskData } = useGetTasksQuery();
-
-  /*const taskData: Object[] = [
-    {
-      id: 1,
-      title: 'Task1',
-      deadline: '12-12-12',
-      bounty: 20,
-      skills: 'Skill1,Skill2',
-      status: 'CREATED',
-      maxParticipants: 4
-    },
-    {
-      id: 2,
-      title: 'Task2',
-      deadline: '12-12-15',
-      bounty: 25,
-      skills: 'Skill1,Skill3',
-      status: 'IN_PROGRESS',
-      maxParticipants: 1
-    },
-    {
-      id: 3,
-      title: 'Task3',
-      deadline: '12-12-18',
-      bounty: 28,
-      skills: 'Skill2,Skill3',
-      status: 'COMPLETED',
-      maxParticipants: 2
-    }
-  ];*/
-
-  console.log(taskData?.data);
 
   const onClick = (id) => console.log(`Clicked on task ${id}`);
   const subheaderProps = {
