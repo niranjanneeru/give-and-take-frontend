34e8066af10becdf88975cf818686b300c1f9355
anandhu013 <56963213+anandhu013@users.noreply.github.com>
2023-08-21 15:35:50 +0530

    Lists completed (#19)
    * added layout and navbar to task list
    
    * Completed both listing pages
    
    * Update subheader.tsx
    
    ---------
    
    Co-authored-by: VaishnavNT <vaishnav.nt.10@gmail.com>
    Co-authored-by: Vaishnav N T <68477941+Vaishnav-NT@users.noreply.github.com>

--- src/pages/task/task.tsx
+++ src/pages/task/task.tsx
@@ -1,10 +1,11 @@
 import { useState } from 'react';
 import './styles.css';
-import Subheader from '../../components/subheader/subheader';
-import TaskListHeader from '../../components/taskListHeader/taskListHeader';
-import TaskListRow from '../../components/taskListRow/taskListRow';
 import { useNavigate } from 'react-router-dom';
 import { useGetTasksQuery } from './api';
+import Layout from '../../components/layout/layout';
+import { useGetUserQuery } from '../employee/api';
+import TableHeader from '../../components/tableHeader/tableHeader';
+import TableRow from '../../components/tableRow/tableRow';
 
 const TaskListPage = () => {
   const [icon] = useState('pencil');
@@ -15,24 +16,32 @@ const TaskListPage = () => {
 
   const onClick = (id) => navigate(`/tasks/${id}`);
 
+  const { data: user } = useGetUserQuery();
+
   const subheaderProps = {
     heading: 'TASKS',
     iconText: 'Create Task',
     iconImg: icon,
-    onClick: () => navigate('/tasks/create')
+    onClick: () => navigate('/tasks/create'),
+    isTask: true
   };
 
   return (
-    <div className='taskList-container'>
-      <Subheader {...subheaderProps}></Subheader>
+    <Layout subheaderProps={subheaderProps} userRole={user?.data.role}>
       <table className='table'>
-        <TaskListHeader></TaskListHeader>
+        <TableHeader userRole={user?.data.role} isTask={true}></TableHeader>
         {taskData &&
           taskData.data.map((task) => (
-            <TaskListRow key={task['id']} task={task} onClick={() => onClick(task['id'])} />
+            <TableRow
+              key={task['id']}
+              row={task}
+              onClick={() => onClick(task['id'])}
+              isTask={true}
+              userRole={user?.data.role}
+            />
           ))}
       </table>
-    </div>
+    </Layout>
   );
 };
 
