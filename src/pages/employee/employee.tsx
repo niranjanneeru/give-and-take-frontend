2afd0672568bb0d1df6cb5c9b1536b675c80c03c
anandhu013 <anandhakrishnan013@gmail.com>
2023-08-23 11:02:13 +0530

    Competed tier badges and direct Bounty

--- src/pages/employee/employee.tsx
+++ src/pages/employee/employee.tsx
@@ -67,7 +67,7 @@ const EmployeePage = () => {
   return (
     <Layout subheaderProps={subheaderProps} userRole={user?.data.role}>
       <table className='table'>
-        <TableHeader userRole={user?.data.role.name} isTask={false}></TableHeader>
+        <TableHeader userRole={user?.data.role} isTask={false}></TableHeader>
         {employeesData &&
           employeesData.data.map((employee) => (
             <TableRow
@@ -79,11 +79,7 @@ const EmployeePage = () => {
                 setOpen(true);
                 setId(employee.id);
               }}
-              // onAward={() => {
-              //   setopenDirectBounty(true);
-              //   setId(employee.id);
-              // }}
-              userRole={user?.data.role.name}
+              userRole={user?.data.role}
               isTask={false}
             />
           ))}
