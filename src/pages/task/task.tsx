c021cf158f36bdbe0ca08b1906ec851fe3384516
Niranjan B <niranjannb7777@gmail.com>
2023-08-23 11:46:12 +0530

    Debounce Search

--- src/pages/task/task.tsx
+++ src/pages/task/task.tsx
@@ -1,10 +1,11 @@
 import './styles.css';
 import { useNavigate } from 'react-router-dom';
-import { useGetTasksQuery } from './api';
+import { useGetTasksQuery, useLazyGetTasksQuery } from './api';
 import Layout from '../../components/layout/layout';
 import { useGetUserQuery } from '../employee/api';
 import TableHeader from '../../components/tableHeader/tableHeader';
 import TableRow from '../../components/tableRow/tableRow';
+import { useEffect, useState } from 'react';
 
 const TaskListPage = () => {
   const navigate = useNavigate();
@@ -24,8 +25,29 @@ const TaskListPage = () => {
     isTask: true
   };
 
+  const [searchText, setSearchText] = useState('');
+
+  const [searchTrigger, { data: searchData, isSuccess: isSearchSuccess }] = useLazyGetTasksQuery();
+
+  useEffect(() => {
+    searchTrigger(searchText);
+  }, [searchText]);
+
+  useEffect(() => {
+    if (isSearchSuccess) console.log(searchData);
+  }, [isSearchSuccess]);
+
+  const searchBarProps = {
+    setSearchText
+  };
+
+
   return (
-    <Layout subheaderProps={subheaderProps} userRole={user?.data.role}>
+    <Layout
+      searchBarProps={searchBarProps}
+      subheaderProps={subheaderProps}
+      userRole={user?.data.role}
+    >
       <div className='taskList-container'>
         <table className='table'>
           <TableHeader userRole={user?.data.role} isTask={true}></TableHeader>
