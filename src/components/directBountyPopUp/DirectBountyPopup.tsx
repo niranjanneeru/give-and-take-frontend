8b85f1798ac32e8ebef5050f81c4650d9f65e09d
NIRANJAN B <niranjannb7777@gmail.com>
2023-08-25 10:14:59 +0530

    Geist (#75)
    * Fixes
    
    * Mark DOwn
    
    * Material Tooltip Integrated
    
    * tooltip added
    
    * fix button design
    
    ---------
    
    Co-authored-by: VaishnavNT <vaishnav.nt.10@gmail.com>

--- src/components/directBountyPopUp/DirectBountyPopup.tsx
+++ src/components/directBountyPopUp/DirectBountyPopup.tsx
@@ -3,6 +3,7 @@ import './DirectBountyPopup.css';
 import PopupInput from '../popupInput/PopupInput';
 import TextArea from '../textArea/TextArea';
 import Button from '../button/button';
+import Tooltip from '@material-ui/core/Tooltip';
 
 type DirectBountyPopupType = {
   onClose: () => void;
@@ -42,7 +43,9 @@ const DirectBountyPopup: FC<DirectBountyPopupType> = ({
   return (
     <div className='modal'>
       <div className='modal-content1'>
-        <img src='assets/icons/close.svg' className='close' onClick={onClose} />
+        <Tooltip title={'Close'} arrow placement='bottom'>
+          <img src='assets/icons/close.svg' className='close' onClick={onClose} />
+        </Tooltip>
         <div className={class_popup}>{popup_text}</div>
         <div className='award-bounty-form'>
           <div className='bounty-div'>
--- src/components/tableRow/tableRow.tsx
+++ src/components/tableRow/tableRow.tsx
@@ -3,6 +3,8 @@ import './tableRow.css';
 import Status from '../status/status';
 import SkillBlock from '../skillBlock/skillBlock';
 import TierLogos from '../tierLogos/tierLogos';
+import Tooltip from '@material-ui/core/Tooltip';
+
 type tableRowProps = {
   row: Object;
   onClick: (e) => void;
@@ -50,41 +52,50 @@ const TableRow: FC<tableRowProps> = ({ row, userRole, pageType, onClick, onDelet
         </td>
       )}
       {/* render actions on employee list page */}
+
       {userRole == 'HR' && pageType == 'employeeList' && (
         <td className='img-td'>
-          <img
-            src={`assets/icons/delete.svg`}
-            onClick={(e) => {
-              onDelete();
-              e.stopPropagation();
-            }}
-          />
-          <img
-            src={`assets/icons/pencil-edit.svg`}
-            onClick={(e) => {
-              onEdit();
-              e.stopPropagation();
-            }}
-          />
+          <Tooltip title='Delete Employee' arrow placement='top'>
+            <img
+              src={`assets/icons/delete.svg`}
+              onClick={(e) => {
+                onDelete();
+                e.stopPropagation();
+              }}
+            />
+          </Tooltip>
+          <Tooltip title={'Edit Employee'} arrow placement='top'>
+            <img
+              src={`assets/icons/pencil-edit.svg`}
+              onClick={(e) => {
+                onEdit();
+                e.stopPropagation();
+              }}
+            />
+          </Tooltip>
         </td>
       )}
       {/* render actions on redeem requests list page */}
       {userRole == 'HR' && pageType == 'redeemRequestsList' && (
         <td className='img-td'>
-          <img
-            src={`assets/icons/check.svg`}
-            onClick={(e) => {
-              onEdit();
-              e.stopPropagation();
-            }}
-          />
-          <img
-            src={`assets/icons/cross.svg`}
-            onClick={(e) => {
-              onDelete();
-              e.stopPropagation();
-            }}
-          />
+          <Tooltip title={'Approve request'} arrow placement='top'>
+            <img
+              src={`assets/icons/accept.svg`}
+              onClick={(e) => {
+                onEdit();
+                e.stopPropagation();
+              }}
+            />
+          </Tooltip>
+          <Tooltip title={'Reject request'} arrow placement='top'>
+            <img
+              src={`assets/icons/reject.svg`}
+              onClick={(e) => {
+                onDelete();
+                e.stopPropagation();
+              }}
+            />
+          </Tooltip>
         </td>
       )}
     </tr>
