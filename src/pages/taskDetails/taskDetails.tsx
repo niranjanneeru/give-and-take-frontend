483bcad1b7c0de7455f88fa092e39b0802db1a5b
anandhu013 <56963213+anandhu013@users.noreply.github.com>
2023-08-25 10:17:30 +0530

    Commentbox access and participants list (#73)
    * Fixed comment input
    
    * Added employees to task
    
    * Adding employees to task fixed
    
    * Fixed task detail page
    
    * Fixed participants list
    
    * Handling maxParticipants at frontend

--- src/pages/taskDetails/taskDetails.tsx
+++ src/pages/taskDetails/taskDetails.tsx
@@ -107,6 +107,9 @@ const TaskDetails = () => {
       handleDelete(id);
     }
   };
+  const isAssignee = taskData?.data.assignees.find((assignee) => assignee.id === user?.data.id)
+    ? true
+    : false;
 
   useEffect(() => {
     if (isFileUploadSuccess) setFileUrl(fileData.data.url);
@@ -174,8 +177,10 @@ const TaskDetails = () => {
                       />
                     );
                   })}
+                {!isApproved && (isAssignee || user?.data.role === 'LEAD') && (
+                  <CommentInput sendComment={sendComment} uploadFile={uploadFile} />
+                )}
               </div>
-              {!isApproved && <CommentInput sendComment={sendComment} uploadFile={uploadFile} />}
             </div>
           )}
         </>
